35fbfb23ec091387f5b90b987b77d548cdaed085
Aaron Lun <alun@wehi.edu.au>
2017-08-12 16:40:16 +0100

    Moved code into a R package for usability.

--- src/poisson_model/package/R/model_fitter.R
+++ src/poisson_model/package/R/model_fitter.R
@@ -1,10 +1,9 @@
-library(Matrix)
-
 computePValue <- function(m, lower=250, tol=0.5, npts=10000) 
 # A function to compute a non-ambient p-value for each barcode.
 # 
 # written by Aaron Lun
-# created 1 August 2017    
+# created 1 August 2017
+# last modified 12 August 2017
 {
     discard <- rowSums(m) == 0
     m <- m[!discard,]
@@ -14,7 +13,7 @@ computePValue <- function(m, lower=250, tol=0.5, npts=10000)
     ambient <- (umi.sum <= lower) # lower => "T" in the text.
     ambient.cells <- m[,ambient]
     ambient.prof <- rowSums(ambient.cells)
-    ambient.prop <- edgeR::goodTuringProportions(ambient.prof)
+    ambient.prop <- goodTuringProportions(ambient.prof)
 
     # Removing cells from the ambient.
     obs <- m[,!ambient]
@@ -46,30 +45,21 @@ computePValue <- function(m, lower=250, tol=0.5, npts=10000)
     sim.totals <- 2^seq(from=lower.pt, to=upper.pt, length.out=S)
 
     # Computing the deviance estimate for simulated runs.
-    sim.LR <- numeric(S)
-    for (x in seq_along(sim.LR)) {
-        cur.means <- ambient.prop*sim.totals[x]
-        current <- rpois(length(ambient.prop), lambda=cur.means)
-        sim.LR[x] <- sum(current * log(current/cur.means), na.rm=TRUE) + sum(cur.means - current)
-    }
+    sim.LR <- .Call("calculate_random_dev", sim.totals, ambient.prop)
 
     # Modelling the total-dependent trend in the simulated LR (and the variance around the trend).
-    log.totals <- log(sim.totals)
+    log.totals <- log2(sim.totals)
     trend.fit <- lowess(x=log.totals, y=sim.LR, f=0.2)
-    spread <- sim.LR/trend.fit$y
-    expected.LR <- spline(trend.fit$x, trend.fit$y, xout=log(obs.totals))$y  
+    sim.spread <- sim.LR/trend.fit$y
+    expected.LR <- spline(trend.fit$x, trend.fit$y, xout=log2(obs.totals))$y  
+    obs.spread <- obs.LR/expected.LR
 
     # Computing a p-value for each observed value.
-    obs.spread <- obs.LR/expected.LR
-    p <- numeric(length(obs.LR))
-    limited <- logical(length(obs.LR))
-    for (x in seq_along(p)) {
-        current <- obs.totals[x]*sqrt(2) >= sim.totals & obs.totals[x]/sqrt(2) <= sim.totals
-        nexceed <- sum(spread[current] >= obs.spread[x])
-        limited[x] <- nexceed==0L
-        p[x] <- (nexceed + 1)/(sum(current)+1)
-    }
+    perm.stats <- .Call("calculate_pval", obs.totals, obs.spread, sim.totals, sim.spread, 2^-tol)
+    limited <- perm.stats[[1]]==0L
+    p <- (perm.stats[[1]]+1)/(perm.stats[[2]]+1)
 
+    # Collating into some sensible output.
     all.p <- all.lr <- all.exp <- rep(NA_real_, length(ambient))
     all.lim <- rep(NA, length(ambient))
     all.p[!ambient] <- p
